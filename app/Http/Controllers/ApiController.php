ccf8306691813d476af0eb55d260038d2e6f8dd2
ryanhoo <hbkeong95@gmail.com>
2018-03-23 10:53:07 +0800

    checking for attendance when withdraw

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -68,6 +68,108 @@ class ApiController extends Controller
         return response()->json($data);
     }
 
+    //get participation
+    public function getParticipations(Request $request) {
+        //handle individual
+        $individual = array();
+        $invitationCodesSolo = Participation::where('activity_id', $request->input('activity_id'))
+                                ->groupBy('invitation_code')
+                                ->whereNotNull('invitation_code')
+                                ->havingRaw('COUNT(invitation_code) = 1')
+                                ->get(['invitation_code']);
+
+        foreach($invitationCodesSolo as $invitationCodeSolo) {
+            $participant = DB::table('participation')
+                            ->join('user', 'participation.participant_id', '=', 'user.user_id')
+                            ->join('volunteer_profile', 'user.user_id', '=', 'volunteer_profile.user_id')
+                            ->where('participation.invitation_code', $invitationCodeSolo->invitation_code)
+                            ->where(function ($q) {
+                                $q->where('participation.status', 'A')
+                                  ->orWhere('participation.status', 'P')
+                                  ->orWhere('participation.status', 'J');
+                            })
+                            ->select('participation.participation_id', 'user.user_id', 'user.full_name',
+                            'volunteer_profile.total_volunteer_duration', 'volunteer_profile.blacklisted_number',
+                            'participation.status')
+                            ->get()->first();
+
+            if($participant) {
+                if($participant->total_volunteer_duration == 0) {
+                    $participant->category = 'Newbie';
+                }
+                else {
+                    $participant->category = 'Regular';
+                }
+                                
+                $individual[] = $participant;
+            }
+            
+        }
+
+        //handle vip
+        $vips = Participation::whereNull('invitation_code')
+                ->whereNotNull('participant_name')
+                ->get(['participant_name', 'participant_remark']);
+
+        //handle group
+        $groups = array();
+        $groupNumber = 1;
+        $invitationCodesGroup = Participation::where('activity_id', $request->input('activity_id'))
+                            ->groupBy('invitation_code')
+                            ->whereNotNull('invitation_code')
+                            ->havingRaw('COUNT(invitation_code) > 1')
+                            ->get(['invitation_code']);
+
+        foreach($invitationCodesGroup as $invitationCodeGroup) {
+            $participants = DB::table('participation')
+                        ->join('user', 'participation.participant_id', '=', 'user.user_id')
+                        ->join('volunteer_profile', 'user.user_id', '=', 'volunteer_profile.user_id')
+                        ->where('participation.invitation_code', $invitationCodeGroup->invitation_code)
+                        ->where(function ($q) {
+                            $q->where('participation.status', 'A')
+                                ->orWhere('participation.status', 'P')
+                                ->orWhere('participation.status', 'J');
+                        })
+                        ->select('participation.participation_id', 'user.user_id', 'user.full_name',
+                        'volunteer_profile.total_volunteer_duration', 'volunteer_profile.blacklisted_number',
+                        'participation.status')
+                        ->get();
+            
+            if(count($participants) > 0) {
+                foreach($participants as $participant) {
+                    if($participant->total_volunteer_duration == 0) {
+                        $participant->category = 'Newbie';
+                    }
+                    else {
+                        $participant->category = 'Regular';
+                    }
+                }
+
+                $group = [
+                    'groupName' => 'Group '.$groupNumber,
+                    'members' => $participants
+                ];
+
+                $groups[] = $group;
+                $groupNumber++;
+            }
+        }
+
+        $results = [
+            'vip' => $vips,
+            'individual' => $individual,
+            'groups' => $groups
+        ];
+
+        $data = [
+            'message' => 'success',
+            'data' => $results
+        ];
+
+        return $data;
+        //return view('pages.test')->with('data', $data);;
+    }
+
     //login
     public function login(Request $request) {
         $user = User::where('email', $request->input('email'))->get()->first();
@@ -703,13 +805,22 @@ class ApiController extends Controller
             $participation = Participation::where('participation_id', $request->input('participation_id'))->get()->first();
 
             if($participation) {
-                $participation->status = "W";
-                $participation->save();
+                if($participation->status == 'A' || $participation->status == 'P') {
+                    $data = [
+                        'status' => 'fail',
+                        'message' => 'Your attendance had been recorded, cannot withdraw from this activity anymore.'
+                    ];
+                }
+                else {
+                    $participation->status = "W";
+                    $participation->invitation_code = $user->user_id.'INV_'.time();
+                    $participation->save();
 
-                $data = [
-                    'status' => 'success',
-                    'message' => 'You have withdrawn from the selected activity.'
-                ];
+                    $data = [
+                        'status' => 'success',
+                        'message' => 'You have withdrawn from the selected activity.'
+                    ];
+                }
             }
             else {
                 $data = [
